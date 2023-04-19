75916b0210a2134a2bbe84559e9f9e363b18c64f
Parallels <tim.scholtz@outlook.com>
2023-04-19 09:28:10 -0400

    file updates:

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # PYthagoras
 Audio analysis tool designed for research purposes. Pre-process audio data, and produces acoustical audio data.
+
+
+
+# Outputs
+
+## filedata.txt
+Store human readble information abouth the file
+
+## csvGen.csv
+Store single number related data about the audio file in the following format
+
+    number of channels, sample_width, sample_freq, number of samples, lenght of file in seconds
---
+++ main.py
@@ -0,0 +1,114 @@
+import os
+import csv
+import shutil
+import pydub 
+import moviepy.editor
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Recusively looks for all files in a given directory, and returns a list with a file path to them
+def getFilesInFolder(k):
+    ret = []
+    for filename in os.listdir(k):
+        f = os.path.join(k, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            ret+=[f]
+        else:
+            ret+=getFilesInFolder(f)
+     
+    return ret
+
+print("Starting Script") 
+print("___________________________________") 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+#Controls some of the optional print statements
+verbose = True
+
+#Get a list of files paths to all audio files inside InputAudioData
+try:
+    nonDem = getFilesInFolder(dir_path+'/InputAudioData/Non_DementiaFiles')
+except:
+    print("Non dem Files not found")
+    nonDem=[]
+
+try:    
+    Dem = getFilesInFolder(dir_path+'/InputAudioData/DementiaFiles')
+except:
+    print("Dem Files not found")
+    Dem = []
+
+#Create empty directorys for output data
+if os.path.exists(dir_path+'/non_dem_output'):
+    shutil.rmtree(dir_path+'/non_dem_output')
+os.makedirs(dir_path+'/non_dem_output')
+if os.path.exists(dir_path+'/dem_output'):
+    shutil.rmtree(dir_path+'/dem_output')
+os.makedirs(dir_path+'/dem_output')
+
+
+for audio in nonDem:
+    file_name = os.path.basename(audio)
+    file = os.path.splitext(file_name)
+    file_location = audio.removeprefix(dir_path+'/InputAudioData/Non_DementiaFiles')
+    file_type = file[1]
+    wavFile = dir_path+'/non_dem_output/'+file[0]+"/"+file[0]+".wav"
+    os.makedirs(dir_path+'/non_dem_output/'+file[0])
+    f = open(dir_path+'/non_dem_output/'+file[0]+"/filedata.txt", "a")
+    csvGen = open(dir_path+'/non_dem_output/'+file[0]+"/genData.csv", "a")
+    csvWriter = csv.writer(csvGen)
+    f.write("The file came from: "+ file_location)
+    #convert the file to a .wav if needed, and then copy the .wav to be part of the output
+    if(file_type == ".wav"):
+        if(verbose): print("Native Wave File:" + dir_path+'/non_dem_output/'+file[0]+"/"+file_name)
+        shutil.copyfile(audio, dir_path+'/non_dem_output/'+file[0]+"/"+file_name)
+    elif(file_type == ".mp3"):
+        if(verbose): print("Converting Mp3 File:" + dir_path+'/non_dem_output/'+file[0]+"/"+file_name)
+        sound = pydub.AudioSegment.from_mp3(audio)
+        sound.export(wavFile, format="wav")
+    elif(file_type == ".mp4"):
+        if(verbose): print("Converting Mp4 File:" + dir_path+'/non_dem_output/'+file[0]+"/"+file_name)
+        video = moviepy.editor.VideoFileClip(audio)
+        #Extract the Audio
+        audio = video.audio
+        #Export the Audio
+        audio.write_audiofile(wavFile)
+    elif(file_type == ".cha"):
+        if(verbose): print("Converting .cha File:" + dir_path+'/non_dem_output/'+file[0]+"/"+file_name)
+        print(".cha handiling not yet implement")
+
+
+    #Open the wave file and start reading from it
+    with wave.open(wavFile) as input:
+        #Read basic informaiton from the wavefile
+        
+        f.write("_"*15)
+        n_channels = input.getnchannels()
+        f.write("Number of channels  - "+ str(n_channels) )
+        sample_width = input.getsampwidth()
+        f.write("Sample Width - "+ str(sample_width))
+        sample_freq = input.getframerate()
+        f.write("Frame rate  - "+ str(sample_freq))
+        n_samples = input.getnframes()
+        f.write("Number of Frames  - "+ str(n_samples))
+        t_audio = round(n_samples/sample_freq,3)
+        f.write("lenght of Audio sample - " + str(t_audio) )
+        f.write("Compression type  - "+ str(input.getcompname()) + " --- Orignal file type: "+ file[1])
+
+        csvWriter.writerow([n_channels,sample_width,sample_freq,n_samples,t_audio])
+        if(input.getnchannels()>1):
+            input = input.setnchannels(1)
+
+        signal_wave = input.readframes(n_samples)
+        signal_array = np.frombuffer(signal_wave, dtype=np.int16)
+        times = np.linspace(0, n_samples/sample_freq, num=n_samples)
+
+        plt.figure(figsize=(15, 5))
+        plt.plot(times, signal_array)
+        plt.title('Signal Amplitude')
+        plt.ylabel('Signal Value')
+        plt.xlabel('Time (s)')
+        plt.xlim(0, t_audio)
+        plt.savefig("test.png")
+
+    f.close()
